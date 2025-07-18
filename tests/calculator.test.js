e433d0922577256026cbcbc5954c46f08a19e8d1
Rohit Bhojak <rohitbhojak99@gmail.com>
2025-07-18 10:16:08 +0000

    Add tests for calculator

---
+++ tests/calculator.test.js
@@ -0,0 +1,49 @@
+import calculator from "../src/calculator.js";
+
+test("add", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("add", () => {
+  expect(calculator.add(1, 2.3)).toClose(3.3);
+});
+
+test("add", () => {
+  expect(calculator.add(1, -7)).toBe(-6);
+});
+
+test("subtract", () => {
+  expect(calculator.subtract(1, 2)).toBe(-1);
+});
+
+test("subtract", () => {
+  expect(calculator.subtract(3, 2.3)).toClose(0.3);
+});
+
+test("subtract", () => {
+  expect(calculator.subtract(3, -2.3)).toClose(5.3);
+});
+
+test("multiply", () => {
+  expect(calculator.multiply(1, 2)).toBe(2);
+});
+
+test("multiply", () => {
+  expect(calculator.multiply(2, 2.3)).toClose(4.6);
+});
+
+test("multiply", () => {
+  expect(calculator.multiply(-2, 2.3)).toClose(-4.6);
+});
+
+test("divide", () => {
+  expect(calculator.divide(1, 2)).toBe(0.5);
+});
+
+test("divide", () => {
+  expect(calculator.divide(4, 2.3)).toClose(1.8);
+});
+
+test("divide", () => {
+  expect(calculator.divide(-4, 2.3)).toClose(-1.8);
+});
